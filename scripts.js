d520b74beb9489ef7bf0c0c1df3f75a2d2707f07
Muminjon <juramirzaev.muminjon@gmail.com>
2023-12-17 10:21:29 +0100

    nav bar created

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Dark/Light mode toggle functionality
+document.getElementById('toggle').addEventListener('change', function() {
+  const body = document.body;
+  body.classList.toggle('dark-mode');
+});
