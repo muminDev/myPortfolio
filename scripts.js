46f736bac6daea7aa8eaccde49d83785625601f8
Muminjon <juramirzaev.muminjon@gmail.com>
2023-12-17 19:39:24 +0100

    Scroll behavior changed

--- scripts.js
+++ scripts.js
@@ -3,3 +3,15 @@ document.getElementById('toggle').addEventListener('change', function() {
   const body = document.body;
   body.classList.toggle('dark-mode');
 });
+
+// Scroll behavoir
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
