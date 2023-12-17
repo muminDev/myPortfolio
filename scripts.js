36f99501e9fd3b38b4be2912a0bf0f47beef0882
Muminjon <juramirzaev.muminjon@gmail.com>
2023-12-17 20:52:48 +0100

    form created to get contact info

--- scripts.js
+++ scripts.js
@@ -15,3 +15,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+//submission contact info
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzD3nxoa2BVMXkhMz9OUjNwAURzIbD76Odpp3d73vZ90Z-27w1Csdap02a3gppIykyIXg/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById('msg')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = 'Message sent succesfully !';
+        setTimeout(() => {
+          msg.innerHTML = ''
+        }, 3000)
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
